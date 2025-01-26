1c540f7edce7c77d64b8b51961481f0f974e3de7
Nahum_Zewdie_GitHub <97863835+nzewdie-UCCS-Student@users.noreply.github.com>
2025-01-26 12:26:54 -0700

    Created prefab and script for health object.

---
+++ Assets/Scripts/Utilities/HealthPickup.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace GGJ2025
+{
+    /// <summary>
+    /// Increases Health
+    /// </summary>
+    public class HealthPickup
+    {
+        public void OnCollisionEnter2D(Collision2D coll)
+        {
+
+        }
+    }
+}
