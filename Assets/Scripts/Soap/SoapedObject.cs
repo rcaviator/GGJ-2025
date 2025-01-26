286bf6af8fae85ea1f3925fbdcbbd739e1a20d61
Asher Tuggle <ashertuggle@gmail.com>
2025-01-25 18:55:10 -0700

    initial trash animations and collision with player

--- Assets/Scripts/Soap/SoapedObject.cs
+++ Assets/Scripts/Soap/SoapedObject.cs
@@ -7,12 +7,18 @@ namespace GGJ2025
     /// </summary>
     public class SoapedObject : MonoBehaviour
     {
+        private static readonly int SoapedParam = Animator.StringToHash("Soaped");
+
         /// <summary>
         /// When the object is first soaped, add soap visuals and start timer
         /// </summary>
         private void Awake()
         {
-            // TODO: Add soap visuals
+            if (TryGetComponent(out Animator animator))
+            {
+                animator.SetBool(SoapedParam, true);
+            }
+
             // TODO: Add soap "target" to enemy
             // TODO: Start timer for soap to end and this object be "cleaned"
         }
@@ -23,6 +29,14 @@ namespace GGJ2025
         private void OnUnsoap()
         {
             Destroy(this); // Only destroys the script, not the game object
+        }
+
+        private void OnDestroy()
+        {
+            if (TryGetComponent(out Animator animator))
+            {
+                animator.SetBool(SoapedParam, false);
+            }
         }
     }
 }
